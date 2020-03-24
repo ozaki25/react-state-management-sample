8af7fc7c057e497dadcd1949607dafc103893b68
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-24 22:40:39 +0900

    add link component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
     <div>
       <h1>About</h1>
+      <Link to="/home">ホームへ</Link>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Link to="/about">Aboutへ</Link>
     </div>
   );
 }
