f9c887bb9edb106c3840daf1416dbd8309656d8f
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-24 23:36:18 +0900

    update TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function TodoList() {
+  const [todoList, setTodoList] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const onChangeText = event => {
+    setInputText(event.target.value);
+  };
+  const onClick = () => {
+    setTodoList([...todoList, { text: inputText }]);
+    setInputText('');
+  };
   return (
     <div>
       <h1>TodoList</h1>
+      <input onChange={onChangeText} value={inputText} />
+      <button onClick={onClick}>追加</button>
+      {todoList.map((todo, i) => (
+        <p key={i}>{todo.text}</p>
+      ))}
       <p>
         <Link to="/">Homeへ</Link>
         <Link to="/counter">Counterへ</Link>
