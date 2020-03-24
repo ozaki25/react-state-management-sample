2bc2daabe0c4e6af95758e77b5aa592c88ecf1bd
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-25 01:05:32 +0900

    apply redux to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useCounter from '../hooks/useCounter';
+import useTodo from '../hooks/useTodo';
 
 function Home() {
+  const { count } = useCounter();
+  const { todoList } = useTodo();
   return (
     <div>
       <h1>Home</h1>
+      <p>現在のCounterの値: {count}</p>
+      <p>現在のTodoListの件数: {todoList.length}</p>
       <p>
         <Link to="/counter">Counterへ</Link>
         <Link to="/todo-list">TodoListへ</Link>
