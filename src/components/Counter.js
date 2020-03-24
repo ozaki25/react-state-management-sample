aa6389e05fc6c1b23d38e731ece0cea51533a819
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-24 23:36:07 +0900

    update Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Counter() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
       <p>
         <Link to="/">Homeへ</Link>
         <Link to="/todo-list">TodoListへ</Link>
