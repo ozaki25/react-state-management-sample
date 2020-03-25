d0933e5345b0aaa19616367c9d139eb628eaf56e
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-26 01:17:01 +0900

    modify store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import counterReducer from './counterSlice';
 import todoReducer from './todoSlice';
 
 export const store = configureStore({
-  reducer: {
+  reducer: combineReducers({
     counter: counterReducer,
     todo: todoReducer,
-  },
+  }),
 });
